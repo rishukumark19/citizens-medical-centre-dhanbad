e9c1a5ca8a3588f54bf0cc84a773b906246a7945
Rishu Kumar <96676931+rishukumark19@users.noreply.github.com>
2026-08-10 23:38:14 +0530

    feat: add CNAME for cmcdhanbad.co.in custom domain + fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/citizens-medical-centre-dhanbad/',
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: '/',
 })
